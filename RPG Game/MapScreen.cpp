bbcd401a2338ca06a53f1993ffde578be564c9e0
Lawrence Alejo <lawrencealejo@hotmail.se>
2023-07-30 00:42:21 +0200

    map rendering

---
+++ RPG Game/MapScreen.cpp
@@ -0,0 +1,52 @@
+#include "MapScreen.h"
+
+MapScreen::MapScreen(SDL_Renderer* renderer, Hero* hero, int* items) 
+{
+	this->renderer = renderer;
+	this->hero = hero;
+	this->items = items;
+
+	//loop through map using nested loop and clear all values out to be zero(walls)
+	for (int x = 0; x <= 9; x++)
+	{
+		for (int y = 0; y <= 9; y++)
+		{
+			map[x][y] = 0;
+		}
+	}
+}
+
+MapScreen::~MapScreen()
+{
+
+}
+
+void MapScreen::draw()
+{
+	//MAP DRAWING
+	//tile representing size of 1 grid thing from map
+	SDL_Rect tileRect = { 0,0,32,32 };
+	//loop through and draw each grid value from map array
+	for (int x = 0; x <= 9; x++)
+	{
+		for (int y = 0; y <= 9; y++)
+		{
+			//IF is ground, set draw colour to ground colour
+			//ELSE set to wall colour
+			if (map[x][y] == 1)
+			{
+				SDL_SetRenderDrawColor(renderer, 136, 60, 100, 255);
+			}
+			else 
+			{
+				//walls
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			}
+			//MOVE rectangle to grid position with regards to tile width/height
+			tileRect.x = x * tileRect.w;
+			tileRect.y = y * tileRect.h;
+			//draw rectangle to screen using current draw colourl
+			SDL_RenderFillRect(renderer, &tileRect);
+		}
+	}
+}
---
+++ RPG Game/MapScreen.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Hero.h"
+
+using namespace std;
+
+class MapScreen
+{
+public:
+	Hero* hero;//reference hero to built inside main.cpp
+	int* items;//going to use this to reference items[10] from main.cpp
+
+	SDL_Renderer* renderer;
+
+	//2d array representing the map. 0 values = walls, 1 values = can walk/ground
+	int map[10][10];
+
+	MapScreen(SDL_Renderer* renderer, Hero* hero, int* items);
+	~MapScreen();
+
+	void draw();
+};
+
