e866d9dedc9951932d6b1eb4f014f974e5f9e729
Lawrence Alejo <lawrencealejo@hotmail.se>
2023-07-29 23:00:02 +0200

    rendering and subsystem set up

--- RPG Game/main.cpp
+++ RPG Game/main.cpp
@@ -176,7 +176,29 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	//TO DO initialise image, mixer, ttf
+	//initialise subsystems we need e.g sdl image, mixer, ttf
+	//TODO: look up c++ bitwise logic
+	//image- being able to deal with other image types other then bmp. Also lets you play with surface pixel data
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG)
+	{
+		cout << "SDL IMG Init Error: " << IMG_GetError() << endl;
+		system("pause");
+		return 1;
+	}
+	//ttf - text to font, for rendering text to screen
+	if (TTF_Init() != 0)
+	{
+		cout << "SDL TTF Init Error: " << TTF_GetError() << endl;
+		system("pause");
+		return 1;
+	}
+	//mixer- sound and music
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
+	{
+		cout << "SDL Mixer Init Error: " << Mix_GetError() << endl;
+		system("pause");
+		return 1;
+	}
 
 	SDL_Window* window = SDL_CreateWindow("RPG GAME!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 240, SDL_WINDOW_SHOWN);
 	if (window == NULL)
@@ -186,6 +208,7 @@ int main(int argc, char** argv) {
 		system("pause");
 		return 1;
 	}
+	
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == NULL)
@@ -196,21 +219,40 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	SDL_SetRenderDrawColor(renderer, 21, 209, 249, 255);//RGB
-	//clear entire screen with current draw colour
-	SDL_RenderClear(renderer);
+	bool keepLooping = true;
+	//Game Loop
+	while (keepLooping)
+	{
 
-	SDL_Rect rect;
-	rect.x = 10;
-	rect.y = 10;
-	rect.w = 50;
-	rect.h = 50;
-	SDL_SetRenderDrawColor(renderer, 34, 76, 22, 255);
-	//draws filled in rectangle to window using rectangles data
-	SDL_RenderFillRect(renderer, &rect);
 
-	//swaps drawing buffer
-	SDL_RenderPresent(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 21, 209, 249, 255);//RGB
+		//clear entire screen with current draw colour
+		SDL_RenderClear(renderer);
+
+		SDL_Rect rect;
+		rect.x = 10;
+		rect.y = 10;
+		rect.w = 50;
+		rect.h = 50;
+		SDL_SetRenderDrawColor(renderer, 34, 76, 22, 255);
+		//draws filled in rectangle to window using rectangles data
+		SDL_RenderFillRect(renderer, &rect);
+
+		//swaps drawing buffer
+		SDL_RenderPresent(renderer);
+
+		//ticks are milliseconds since the start of SDL init
+		if (SDL_GetTicks() > 5000)//1000ms = 1 second
+		{
+			keepLooping = false;
+		}
+
+	}
+	//Clean Up
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
 
 	//lets user interact by pressing anykey
 	system("pause");
