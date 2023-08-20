5f7af63ae4f5eac8fc2a4ea53db475637b522da1
Lawrence Alejo <lawrencealejo@hotmail.se>
2023-08-20 14:59:04 +0200

    adds header to the BattleScreen.cpp

--- RPG Game/BattleScreen.h
+++ RPG Game/BattleScreen.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <list>
+#include "Hero.h"
+#include "Glob.h"
+#include "Mimic.h"
+//TODO MORE
+
+using namespace std;
+
 class BattleScreen
 {
+public:
+	BattleScreen();
+	~BattleScreen();
 };
 
